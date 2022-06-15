fb50219bcccc806af30d37bf02f41bca74e8b5ca
svendesign <sven@svendesign.nl>
2022-06-15 12:30:01 +0200

    Error handling

--- src/components/cards/EpisodeCard.vue
+++ src/components/cards/EpisodeCard.vue
@@ -56,8 +56,8 @@ export default {
 			overflow: hidden;
 			text-overflow: ellipsis;
 			display: -webkit-box;
-			-webkit-line-clamp: 3;
-			line-clamp: 3;
+			-webkit-line-clamp: 2;
+			line-clamp: 2;
 			-webkit-box-orient: vertical;
 		}
 	}
--- src/stores/shows.js
+++ src/stores/shows.js
@@ -32,11 +32,31 @@ export const useShowsStore = defineStore({
 	},
 	actions: {
 		init() {
-			axios.get("https://api.tvmaze.com/shows").then((response) => {
-				this.allShows = response.data;
-				this.createPopularShows(this.allShows);
-				this.getGenresOfShows(this.allShows);
-			});
+			axios
+				.get("https://api.tvmaze.com/shows")
+				.then((response) => {
+					this.allShows = response.data;
+					this.createPopularShows(this.allShows);
+					this.getGenresOfShows(this.allShows);
+				})
+				.catch((error) => {
+					if (error.response) {
+						// The request was made and the server responded with a status code
+						// that falls out of the range of 2xx
+						console.log(error.response.data);
+						console.log(error.response.status);
+						console.log(error.response.headers);
+					} else if (error.request) {
+						// The request was made but no response was received
+						// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+						// http.ClientRequest in node.js
+						console.log(error.request);
+					} else {
+						// Something happened in setting up the request that triggered an Error
+						console.log("Error", error.message);
+					}
+					console.log(error.config);
+				});
 		},
 
 		createPopularShows(payload) {
